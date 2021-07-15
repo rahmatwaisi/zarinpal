f21ec797ff6d7a4638b3028249637bac154f5e48
mr648 <rahmatwaisi@gmail.com>
2021-07-15 12:39:29 +0430

    initialized zarinpal services

--- src/ZarinpalService.php
+++ src/ZarinpalService.php
@@ -3,8 +3,47 @@
 
 namespace RahmatWaisi\Zarinpal;
 
+use RahmatWaisi\Zarinpal\Services\Payment\AuthorityService;
+use RahmatWaisi\Zarinpal\Services\Payment\PaymentService;
+use RahmatWaisi\Zarinpal\Services\Payment\RefundService;
+use RahmatWaisi\Zarinpal\Services\Payment\UnVerifiedService;
+use RahmatWaisi\Zarinpal\Services\Payment\VerificationService;
 
 class ZarinpalService
 {
 
+    /**
+     * AuthorityService
+     */
+    protected $authorityService;
+
+    /**
+     * PaymentService
+     */
+    protected $paymentService;
+
+    /**
+     * RefundService
+     */
+    protected $refundService;
+
+    /**
+     * UnVerifiedService
+     */
+    protected $unVerifiedService;
+
+    /**
+     * VerificationService
+     */
+    protected $verificationService;
+
+
+    public function __construct()
+    {
+        $this->authorityService = new AuthorityService();
+        $this->paymentService = new PaymentService();
+        $this->refundService = new RefundService();
+        $this->unVerifiedService = new UnVerifiedService();
+        $this->verificationService = new VerificationService();
+    }
 }
